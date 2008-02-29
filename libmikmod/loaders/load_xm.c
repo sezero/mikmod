7ef027487f1dd80c2238b152f22dc335887c592e
denis111 <denis111@users.sourceforge.net>
2008-02-29 18:49:03 +0000

    Support of non-standart header size (not 0x114).

--- libmikmod/loaders/load_xm.c
+++ libmikmod/loaders/load_xm.c
@@ -687,7 +687,7 @@ BOOL XM_Load(BOOL curious)
 		_mm_errno=MMERR_NOT_A_MODULE;
 		return 0;
 	}
-	_mm_read_UBYTES(mh->orders,256,modreader);
+	_mm_read_UBYTES(mh->orders,mh->headersize-20,modreader);
 
 	if(_mm_eof(modreader)) {
 		_mm_errno = MMERR_LOADING_HEADER;
