33ea1f9b7f80c09b912c414a77c5f8b9a6ba7485
Shlomi Fish <shlomif@shlomifish.org>
2012-11-19 15:30:16 +0200

    Fix a CLANG warning.

--- libmikmod/loaders/load_stm.c
+++ libmikmod/loaders/load_stm.c
@@ -330,7 +330,13 @@ static BOOL STM_Load(BOOL curious)
 		q->speed      = (mh->sample[t].c2spd * 8363) / 8448;
 		q->volume     = mh->sample[t].volume;
 		q->length     = mh->sample[t].length;
+#if 0
 		if (/*(!mh->sample[t].volume)||*/(q->length==1)) q->length=0;
+#else
+		if (q->length==1) {
+			q->length=0;
+		}
+#endif
 		q->loopstart  = mh->sample[t].loopbeg;
 		q->loopend    = mh->sample[t].loopend;
 		q->seekpos    = MikMod_ISA;
