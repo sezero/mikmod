eae9a6269c3fb7be24bf133352b2e6732325751a
sezero <sezero@users.sourceforge.net>
2013-08-25 18:28:19 +0300

    drv_wav.c: support for DMODE_FLOAT (WAVE_FORMAT_IEEE_FLOAT) output

--- libmikmod/drivers/drv_wav.c
+++ libmikmod/drivers/drv_wav.c
@@ -53,21 +53,37 @@ static	CHAR *filename=NULL;
 
 static void putheader(void)
 {
+	ULONG rflen = 36;
+	if (md_mode&DMODE_FLOAT)
+		rflen += 2 + 12; /* FmtExt + "fact" chunk sizes */
+	rflen += dumpsize;
+
 	_mm_fseek(wavout,0,SEEK_SET);
 	_mm_write_string("RIFF",wavout);
-	_mm_write_I_ULONG(dumpsize+44-8,wavout);	/* <AWE> must be smaller by 8! */
+	_mm_write_I_ULONG(rflen,wavout);
 	_mm_write_string("WAVEfmt ",wavout);
-	_mm_write_I_ULONG(16,wavout);	/* length of this RIFF block crap */
+	_mm_write_I_ULONG((md_mode&DMODE_FLOAT)?18:16,wavout);	/* length of this RIFF block crap */
 
-	_mm_write_I_UWORD(1, wavout);	/* microsoft format type */
+	_mm_write_I_UWORD((md_mode&DMODE_FLOAT)? 3:1,wavout);	/* WAVE_FORMAT_PCM :1, WAVE_FORMAT_IEEE_FLOAT :3 */
 	_mm_write_I_UWORD((md_mode&DMODE_STEREO)?2:1,wavout);
 	_mm_write_I_ULONG(md_mixfreq,wavout);
-	_mm_write_I_ULONG(md_mixfreq*((md_mode&DMODE_STEREO)?2:1)*
-	                  ((md_mode&DMODE_16BITS)?2:1),wavout);
+	_mm_write_I_ULONG(md_mixfreq *	((md_mode&DMODE_STEREO)?2:1) *
+					((md_mode&DMODE_FLOAT)? 4:
+					 (md_mode&DMODE_16BITS)?2:1), wavout);
 	/* block alignment (8/16 bit) */
-	_mm_write_I_UWORD(((md_mode&DMODE_16BITS)?2:1)*
+	_mm_write_I_UWORD(((md_mode&DMODE_FLOAT)?4:(md_mode&DMODE_16BITS)?2:1)*
 	                  ((md_mode&DMODE_STEREO)?2:1),wavout);
-	_mm_write_I_UWORD((md_mode&DMODE_16BITS)?16:8,wavout);
+	_mm_write_I_UWORD((md_mode&DMODE_FLOAT)?32:(md_mode&DMODE_16BITS)?16:8,wavout);
+
+	if (md_mode&DMODE_FLOAT) {
+		_mm_write_I_UWORD(0,wavout);						/* 0 byte of FmtExt */
+		_mm_write_string("fact",wavout);
+		_mm_write_I_ULONG(4,wavout);
+		_mm_write_I_ULONG(dumpsize / ((md_mode&DMODE_STEREO)?2:1) /		/* # of samples written */
+					     ((md_mode&DMODE_FLOAT)? 4:
+					      (md_mode&DMODE_16BITS)?2:1), wavout);
+	}
+
 	_mm_write_string("data",wavout);
 	_mm_write_I_ULONG(dumpsize,wavout);
 }
@@ -149,12 +165,18 @@ static void WAV_Update(void)
 
 	done=VC_WriteBytes(audiobuffer,BUFFERSIZE);
 
-/*    <AWE> Fix for 16bit samples on big endian systems:				*/
-/*	      Just swap bytes via "_mm_write_I_UWORDS ()" if we have 16 bit output.	*/
-        if (md_mode & DMODE_16BITS)
-            _mm_write_I_UWORDS((UWORD *) audiobuffer,done>>1,wavout);
-        else
-            _mm_write_UBYTES(audiobuffer,done,wavout);
+	if (md_mode & DMODE_FLOAT) {
+	/* O.S. - assuming same endian model for integer vs fp values	*/
+		_mm_write_I_ULONGS((ULONG *) audiobuffer,done>>2,wavout);
+	}
+	else if (md_mode & DMODE_16BITS) {
+	/* <AWE> Fix for 16bit samples on big endian systems: Just swap
+	 * bytes via "_mm_write_I_UWORDS ()" if we have 16 bit output.	*/
+		_mm_write_I_UWORDS((UWORD *) audiobuffer,done>>1,wavout);
+	}
+	else {
+		_mm_write_UBYTES(audiobuffer,done,wavout);
+	}
 	dumpsize+=done;
 }
 
