55621d07c3f8191cb92c17a63f2c4158c5f53f98
sezero <sezero@users.sourceforge.net>
2017-12-14 23:56:00 +0300

    removed old mikmod directories (they still live in the old-mikmod branch)

--- old-mikmod/mikmod/clean.bat
+++
@@ -1,1 +0,0 @@
-del /s *.opt *.mk1 *.mk *.lb1 *.lk1 *.obj *.lib *.err *.map *.sym *.bak *.exe *.pdb *.ncb *.plg ugh.* *.ilk *.pch *.idb
--- old-mikmod/mikmod/wcmake.bat
+++
@@ -1,5 +0,0 @@
-cd dos
-wmake %1 -f dos.wc
-cd..
-wmake %1 -f miklib.wc
-wmake %1 -f makefile.wc
