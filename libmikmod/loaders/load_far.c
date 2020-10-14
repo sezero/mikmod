ff42bd6817e0438738a57889311bc8efd427310a
AliceLR <petrifiedrowan@gmail.com>
2020-10-14 17:38:08 -0600

    Fix handling of .FAR pattern break byte

--- libmikmod/loaders/load_far.c
+++ libmikmod/loaders/load_far.c
@@ -245,12 +245,14 @@ static BOOL FAR_Load(BOOL curious)
 	if(!AllocPatterns()) return 0;
 
 	for(t=0;t<of.numpat;t++) {
-		UBYTE rows=0;
+		UWORD rows=0;
 		UBYTE tempo;
 
 		memset(pat,0,256*16*4*sizeof(FARNOTE));
 		if(mh2->patsiz[t]) {
-			rows  = _mm_read_UBYTE(modreader);
+			/* Break position byte is always 1 less than the final row index,
+			   i.e. it is 2 less than the total row count. */
+			rows  = _mm_read_UBYTE(modreader) + 2;
 			tempo = _mm_read_UBYTE(modreader);
 			(void)tempo; /* unused */
 
